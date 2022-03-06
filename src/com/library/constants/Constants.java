f5386c47c4e074611c47678d118ddbb8bc52576e
KHADRI <khadri.institutions@gmail.com>
2022-03-06 15:38:09 +0530

    change in constants file

--- src/com/library/constants/Constants.java
+++ src/com/library/constants/Constants.java
@@ -6,6 +6,6 @@ public static final String STUDENTFILEPATH = "C:\\Library\\student\\";
 public static final String STUDENTNAME = "Enter Your Name";
 public static final String STUDENTBRANCH = "Enter Your Branch";
 public static final String STUDENTROLLNUMBER = "Enter Your RollNumber";
-public static final String STUDENTSTUDINGYEAR = "Enter Your Current Study Yaer";
+public static final String STUDENTSTUDINGYEAR = "Enter Your Current Study Year";
 
 }
